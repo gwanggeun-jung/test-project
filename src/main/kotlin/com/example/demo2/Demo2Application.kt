e6cbec5bdccdee4d4a2b4dc0c99fecafbf1d7a99
GwangGeun-Jung <gwanggeun.jung@deliveryhero.com>
2022-10-11 16:29:11 +0200

    fisdf

---
+++ src/main/kotlin/com/example/demo2/Demo2Application.kt
@@ -0,0 +1,12 @@
+package com.example.demo2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Demo2Application
+
+// hee
+fun main(args: Array<String>) {
+	runApplication<Demo2Application>(*args)
+}
